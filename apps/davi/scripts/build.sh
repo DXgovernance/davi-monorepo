d326e9c27ac0f37508b5894aa89223ff69acaf5c
Milton Tulli <milton.tulli@gmail.com>
2023-01-06 16:19:56 -0300

    Add build action

--- apps/davi/scripts/build.sh
+++ apps/davi/scripts/build.sh
@@ -5,4 +5,4 @@
 # export REACT_APP_GIT_SHA=$(echo $(git rev-parse  HEAD) | cut -c1-9)
 export REACT_APP_VERSION=$npm_package_version
 export SKIP_PREFLIGHT_CHECK=true
-yarn react-app-rewired --max_old_space_size=4096 build
+pnpm react-app-rewired --max_old_space_size=4096 build

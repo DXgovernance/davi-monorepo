0701cfc845b52a4adfb1dcf60b8c5266feded53b
Milton Tulli <milton.tulli@gmail.com>
2023-01-06 16:34:16 -0300

    Update local config before build

--- apps/davi/scripts/build.sh
+++ apps/davi/scripts/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-# node scripts/beforeBuild.js
+echo "Executing beforeBuild"
+node scripts/beforeBuild.js
+
+echo "Updating DAVI bytecodes"
+node scripts/updateBytecodes.js
 # yarn hardhat compile
 # Add in once we upgrade to support ordered environment variables
 # export REACT_APP_GIT_SHA=$(echo $(git rev-parse  HEAD) | cut -c1-9)

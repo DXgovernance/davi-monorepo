215bcb9456a23fbdd691ca56a02165bfe57dc18e
rossneilson <rossneilson99@gmail.com>
2023-01-10 15:50:53 +0000

    fix: Fixing context use for orbis

--- apps/davi/src/contexts/Guilds/orbis.tsx
+++ apps/davi/src/contexts/Guilds/orbis.tsx
@@ -71,4 +71,4 @@ export const OrbisProvider = ({ children }) => {
   );
 };
 
-export const useOrbisContext = useContext(OrbisContext);
+export const useOrbisContext = () => useContext(OrbisContext);

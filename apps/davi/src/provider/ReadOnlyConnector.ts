2822ba28ed051317b6bd4e8c0c65c01b8db2b5e4
rossneilson <rossneilson99@gmail.com>
2023-01-10 14:01:44 +0000

    fix: Adding back else

--- apps/davi/src/provider/ReadOnlyConnector.ts
+++ apps/davi/src/provider/ReadOnlyConnector.ts
@@ -113,7 +113,7 @@ export class ReadOnlyConnector extends Connector<
 
   protected onAccountsChanged = (accounts: string[]) => {
     if (accounts.length === 0) this.emit('disconnect');
-    this.emit('change', { account: null });
+    else this.emit('change', { account: null });
   };
 
   protected onChainChanged = (chainId: number | string) => {

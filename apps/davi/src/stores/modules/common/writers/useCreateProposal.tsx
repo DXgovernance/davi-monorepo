0facdd2f64866991a496101d9d621fba49f9e707
Dino <crescimbenidino@gmail.com>
2023-01-18 16:59:31 -0300

    feat: handled proposal creation error, defined args and moved validation to hook

--- apps/davi/src/stores/modules/common/writers/useCreateProposal.tsx
+++ apps/davi/src/stores/modules/common/writers/useCreateProposal.tsx
@@ -1,26 +1,40 @@
 import { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
+import { isValidGuildProposal } from 'utils';
 import { WriterHooksInteface } from 'stores/types';
 import { useTransactions } from 'contexts/Guilds';
 import { useERC20Guild } from 'hooks/Guilds/contracts/useContract';
 
-type UseCreateProposalInterface = WriterHooksInteface['useCreateProposal'];
+type IUseCreateProposal = WriterHooksInteface['useCreateProposal'];
+type IHandleCreateProposal = ReturnType<IUseCreateProposal>;
 
-export const useCreateProposal: UseCreateProposalInterface = (
-  daoAddress: string
-) => {
+export const useCreateProposal: IUseCreateProposal = daoAddress => {
   const { t } = useTranslation();
   const daoContract = useERC20Guild(daoAddress);
   const { createTransaction } = useTransactions();
 
-  const handleCreateProposal = useCallback(
+  const handleCreateProposal: IHandleCreateProposal = useCallback(
     async (
-      title: string,
-      proposalData: any,
-      cb: (error?: any, txtHash?: any) => void = null
+      title,
+      description,
+      toArray,
+      dataArray,
+      valueArray,
+      totalOptions,
+      otherFields,
+      cb
     ) => {
-      const { toArray, dataArray, valueArray, totalOptions, contentHash } =
-        proposalData;
+      const { contentHash } = otherFields;
+      const { isValid, error } = isValidGuildProposal({
+        toArray,
+        dataArray,
+        valueArray,
+        totalOptions,
+        title,
+        contentHash,
+      });
+
+      if (!isValid) throw Error(error);
 
       createTransaction(
         `${t('createProposal')} ${title}`,
@@ -31,7 +45,7 @@ export const useCreateProposal: UseCreateProposalInterface = (
             valueArray,
             totalOptions,
             title,
-            `${contentHash}`
+            `${otherFields.contentHash}`
           );
         },
         true,

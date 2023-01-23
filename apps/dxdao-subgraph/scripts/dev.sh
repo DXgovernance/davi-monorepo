0d5e197b0cf324fc92d317f4013e8d947a52d142
Milton Tulli <milton.tulli@gmail.com>
2023-01-23 10:50:27 -0300

    Refactor subgraph dev script

--- apps/dxdao-subgraph/scripts/dev.sh
+++ apps/dxdao-subgraph/scripts/dev.sh
@@ -44,25 +44,31 @@ safeDockerStart(){
 
 }
 
-hardhat_running() {
+isHardhatRunning() {
   nc -z localhost 8545
 }
 
 waitForHardhat(){
     retry_count=0
-    while ! hardhat_running; do
+    while ! isHardhatRunning; do
         if [ $retry_count -eq $MAX_RETRY ]; then
             echo "Hardhat Node is not ready after $MAX_RETRY retries. Exiting script"
             exit 1
         fi
         echo "Hardhat Node is not ready. Sleeping. Attempt $retry_count of $MAX_RETRY"
-        sleep 1 # wait for 1 second before check again
+        sleep 1
         retry_count=$((retry_count+1))
     done
 }
 
 waitForGraphContainer(){
+    retry_count=0
     while true; do
+        if [ $retry_count -eq $MAX_RETRY ]; then
+            echo "Hardhat Node is not ready after $MAX_RETRY retries. Exiting script"
+            exit 1
+        fi
+
         status=$(docker ps --filter "name=dxdao-subgraph-graph-node*" -q | xargs -I {} docker inspect --format '{{.State.Status}}' {})
         if [[ $status == *"running"* ]]; then
             echo "Graph node container is running"
@@ -70,6 +76,30 @@ waitForGraphContainer(){
         else
             echo "Waiting for graph-node to start"
             sleep 1
+            retry_count=$((retry_count+1))
+        fi
+    done
+}
+
+startGraphQlPlayground(){
+    retry_count=0
+    while true; do
+        if [ $retry_count -eq $MAX_RETRY ]; then
+            echo "Hardhat Node is not ready after $MAX_RETRY retries. Exiting script"
+            exit 1
+        fi
+        status_code=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8000/subgraphs/name/mprasanjith/dxdao/graphql)
+        if [[ $status_code -eq 200 ]]; then
+            echo "Subgraph::: Local deployment ready!. Opening graphql playground in Browser"
+            if [[ "$(uname -s)" == "Darwin" ]]; then
+                open http://127.0.0.1:8000/subgraphs/name/mprasanjith/dxdao
+            elif [[ "$(uname -s)" == "Linux" ]]; then
+                xdg-open http://127.0.0.1:8000/subgraphs/name/mprasanjith/dxdao
+            fi
+            break
+        else
+            sleep 1
+            retry_count=$((retry_count+1))
         fi
     done
 }
@@ -92,18 +122,6 @@ docker compose up &
 waitForGraphContainer
 
 echo "Subgraph::: Creating graph and deploying local"
-pnpm run start-local
+pnpm run start-local &
 
-
-while true; do
-    status_code=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8000/subgraphs/name/mprasanjith/dxdao/graphql)
-    if [[ $status_code -eq 200 ]]; then
-        echo "Subgraph::: Local deployment ready!. Opening graphql playground in Browser"
-        if [[ "$(uname -s)" == "Darwin" ]]; then
-            open http://127.0.0.1:8000/subgraphs/name/mprasanjith/dxdao
-        elif [[ "$(uname -s)" == "Linux" ]]; then
-            xdg-open http://127.0.0.1:8000/subgraphs/name/mprasanjith/dxdao
-        fi
-        break
-    fi
-done
+startGraphQlPlayground

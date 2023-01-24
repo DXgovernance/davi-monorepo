1fdd0ad66ffd929f014e30d58d84092d8e4bf67a
Tomas Pulenta <pulen@Tomass-MacBook-Pro.local>
2023-01-24 19:14:53 -0300

    adding styles to votes on proposal

--- apps/davi/src/components/ProposalVotesCard/ ProposalVotesCard.tsx
+++ apps/davi/src/components/ProposalVotesCard/ ProposalVotesCard.tsx
@@ -1,33 +1,91 @@
-import { InfoDetail } from 'components/ProposalInfoCard/ProposalInfoCard.styled';
-import { SidebarCard, SidebarCardContent } from 'components/SidebarCard';
+import {
+  SidebarCard,
+  SidebarCardContent,
+  SidebarCardHeaderSpaced,
+} from 'components/SidebarCard';
+import { SidebarCardContentWrapper } from 'components/SidebarCard/SidebarCard.styled';
 import { useTranslation } from 'react-i18next';
+import { shortenAddress } from 'utils';
+import {
+  Amount,
+  InfoDetail,
+  VoteOption,
+  VoteOptionWrapper,
+  VotesAmount,
+  VotesAmountWrapper,
+} from './ProposalVotesCard.styled';
 
 const ProposalVotesCard: React.FC = () => {
   const { t } = useTranslation();
 
   const votes = [
     {
-      address: 'charles.eth',
+      voter: '0x4e91c9F086DB2Fd8aDb1888e9b18e17F70B7BdB6',
       vote: 'For',
-      amount: '1,89%',
+      votingPower: '1,89%',
     },
     {
-      address: '0x64e1...90E0',
+      voter: '0xC5B20AdE9c9Cd5e0CC087C62b26B815A4bc1881f',
       vote: 'For',
-      amount: '1,89%',
+      votingPower: '1,89%',
+    },
+    {
+      voter: '0xC5B20AdE9c9Cd5e0CC087C62b26B815A4bc1881f',
+      vote: 'Against',
+      votingPower: '1,89%',
+    },
+    {
+      voter: '0xC5B20AdE9c9Cd5e0CC087C62b26B815A4bc1881f',
+      vote: 'For',
+      votingPower: '1,89%',
+    },
+    {
+      voter: '0xC5B20AdE9c9Cd5e0CC087C62b26B815A4bc1881f',
+      vote: 'For',
+      votingPower: '1,89%',
+    },
+    {
+      voter: '0xC5B20AdE9c9Cd5e0CC087C62b26B815A4bc1881f',
+      vote: 'For',
+      votingPower: '1,89%',
+    },
+    {
+      voter: '0xC5B20AdE9c9Cd5e0CC087C62b26B815A4bc1881f',
+      vote: 'For',
+      votingPower: '1,89%',
+    },
+    {
+      voter: '0xC5B20AdE9c9Cd5e0CC087C62b26B815A4bc1881f',
+      vote: 'For',
+      votingPower: '1,89%',
     },
   ];
   return (
-    <SidebarCard header={t('votes')}>
-      <SidebarCardContent>
-        {votes?.map(vote => (
-          <InfoDetail>
-            <span>{vote.address}</span>
-            <span>{vote.vote}</span>
-            <span>{vote.amount}</span>
-          </InfoDetail>
-        ))}
-      </SidebarCardContent>
+    <SidebarCard
+      header={
+        <>
+          <SidebarCardHeaderSpaced>
+            {t('votes')}
+            <VotesAmountWrapper>
+              <VotesAmount>{votes.length}</VotesAmount>
+            </VotesAmountWrapper>
+          </SidebarCardHeaderSpaced>
+        </>
+      }
+    >
+      <SidebarCardContentWrapper>
+        <SidebarCardContent>
+          {votes?.map(vote => (
+            <InfoDetail>
+              <span>{shortenAddress(vote.voter)}</span>
+              <VoteOptionWrapper>
+                <VoteOption>{vote.vote}</VoteOption>
+              </VoteOptionWrapper>
+              <Amount>{vote.votingPower}</Amount>
+            </InfoDetail>
+          ))}
+        </SidebarCardContent>
+      </SidebarCardContentWrapper>
     </SidebarCard>
   );
 };
---
+++ apps/davi/src/components/ProposalVotesCard/ProposalVotesCard.styled.tsx
@@ -0,0 +1,43 @@
+import { Box } from 'components/primitives/Layout';
+import styled from 'styled-components';
+
+export const InfoDetail = styled(Box)`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 0.75rem;
+`;
+
+export const VoteOptionWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+  margin-right: 1rem;
+`;
+
+export const VotesAmountWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: flex-start;
+  margin-left: 1rem;
+`;
+
+export const VotesAmount = styled.div`
+  border-radius: 8px;
+  padding: 0px 6px;
+  background-color: ${({ theme }) => theme.colors.darkGreen1};
+`;
+
+export const VoteOption = styled.div`
+  background-color: ${({ theme }) => theme.colors.darkGreen1};
+  border-radius: 8px;
+  padding: 2px 6px;
+`;
+
+export const Amount = styled.div`
+  color: #a1a6b0;
+`;
+
+export const VotesAMount = styled.div`
+  color: black;
+`;

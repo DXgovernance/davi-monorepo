90f929c955a311fca9b70f3765197605289d4398
dcrescimbeni <crescimbenidino@gmail.com>
2023-01-27 15:43:03 -0300

    feat(subgraph): added isToken to permission schema

--- apps/dxdao-subgraph/src/mappings/PermissionRegistry/mapping.ts
+++ apps/dxdao-subgraph/src/mappings/PermissionRegistry/mapping.ts
@@ -1,8 +1,12 @@
 import { BigInt } from '@graphprotocol/graph-ts';
 import { PermissionSet } from '../../types/PermissionRegistry/PermissionRegistry';
-import { Guild, GuildPermission } from '../../types/schema';
+import { GuildPermission } from '../../types/schema';
 
 export function handleSetPermission(event: PermissionSet): void {
+  const ANY_FUNC_SIGNATURE = '0x00000000';
+  const ERC20_TRANSFER_SIGNATURE = '0xa9059cbb';
+  const ERC20_APPROVE_SIGNATURE = '0x095ea7b3';
+
   let id =
     event.params.from.toHexString() +
     '-' +
@@ -15,9 +19,19 @@ export function handleSetPermission(event: PermissionSet): void {
     permission = new GuildPermission(id);
   }
 
+  let isToken = false;
+  if (
+    event.params.functionSignature.toHexString() === ANY_FUNC_SIGNATURE ||
+    event.params.functionSignature.toHexString() === ERC20_APPROVE_SIGNATURE ||
+    event.params.functionSignature.toHexString() === ERC20_TRANSFER_SIGNATURE
+  ) {
+    isToken = true;
+  }
+
   permission.from = event.params.from.toHexString();
   permission.to = event.params.to.toHexString();
   permission.functionSignature = event.params.functionSignature;
+  permission.isToken = isToken;
   permission.valueAllowed = event.params.value;
   permission.fromTime = event.params.fromTime;
   permission.allowed = !(

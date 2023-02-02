e2f5a0b0e7d793a9525580e3bf3c59f8cc910a49
Kenny-Gin1 <kenny.chung@live.se>
2023-02-02 19:23:45 +0100

    delete: removed comment

--- apps/davi/src/stores/modules/common/fetchers/useGetNumberOfActiveProposals.ts
+++ apps/davi/src/stores/modules/common/fetchers/useGetNumberOfActiveProposals.ts
@@ -1,10 +1,6 @@
 import { useContractRead } from 'wagmi';
 import { BaseERC20Guild } from 'contracts/ts-files/BaseERC20Guild';
 import { useListenToProposalStateChanged } from '../events/useListenToProposalStateChanged';
-// import { FetcherHooksInterface } from 'stores/types';
-
-// type IUseGetNumberOfActiveProposals =
-//   FetcherHooksInterface['useGetNumberOfActiveProposals'];
 
 export const useGetNumberOfActiveProposals = (daoId: string) => {
   const { data, refetch, ...rest } = useContractRead({

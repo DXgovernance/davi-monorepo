3d825bea8de5d67260a8c6fd6a4142b61d2b2b10
Milton Tulli <milton.tulli@gmail.com>
2023-02-03 17:10:47 -0300

    Fix hardhat cleanup

--- apps/dev-scripts/src/dev.sh
+++ apps/dev-scripts/src/dev.sh
@@ -3,26 +3,12 @@
 # Exit script as soon as a command fails.
 set -o errexit
 
-# Executes cleanup function at script exit.
-# trap cleanup EXIT
-
-# cleanup() {
-#   # Kill the hardhat instance that we started (if we started one and if it's still running).
-#   if [ -n "$hardhat_pid" ] && ps -p $hardhat_pid > /dev/null; then
-#     kill -9 $hardhat_pid
-#   fi
-# }
-
 hardhat_running() {
   nc -z localhost 8545
 }
 
-
 start_hardhat_node() {
   pnpm hardhat node --hostname 0.0.0.0 --export "build/deployment-info.json" | grep -vE 'eth_getBlockByNumber|eth_getBlockByHash|eth_getTransactionReceipt|Mined empty block|eth_getLogs|eth_call|Transaction|From|To|Value|Gas used|Block|Contract deployment|Contract address|Contract call|eth_chainId|eth_blockNumber|eth_accounts' &
-  
-  # hardhat_pid=$!
-  # wait $grep_pid
 
   echo "Waiting for hardhat to launch..."
 

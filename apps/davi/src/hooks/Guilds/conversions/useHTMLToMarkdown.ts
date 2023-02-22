cf041af12b301ef505f3e9c330f8eef60b6fa7c2
Yathindra <yathindrarawya123@gmail.com>
2023-02-22 18:28:12 +0530

    fix the html to markdown conversion by showdown

--- apps/davi/src/hooks/Guilds/conversions/useHTMLToMarkdown.ts
+++ apps/davi/src/hooks/Guilds/conversions/useHTMLToMarkdown.ts
@@ -1,13 +1,12 @@
 import { useMemo } from 'react';
-import showdown from 'showdown';
+import TurndownService from 'turndown';
 
 export default function useHTMLToMarkdown(html: string) {
-  const converter: showdown.Converter = new showdown.Converter();
-
+  const turndownService = new TurndownService();
   const htmlParsed = useMemo(() => {
     if (!html) return '';
 
-    return converter && converter.makeMarkdown(html);
+    return turndownService && turndownService.turndown(html);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [html]);
 

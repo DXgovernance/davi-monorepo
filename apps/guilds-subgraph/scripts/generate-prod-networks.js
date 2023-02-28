67f0981008343ea2c2a156fef011e2f88bfa5fc9
Madusha Prasanjith <hi@madusha.me>
2023-02-28 19:30:56 +0100

    feat: Use env to fetch graph deployments on GC.

--- apps/guilds-subgraph/scripts/generate-prod-networks.js
+++ apps/guilds-subgraph/scripts/generate-prod-networks.js
@@ -5,20 +5,6 @@ fs.writeFileSync(
   path.resolve(__dirname, '../networks.json'),
   JSON.stringify(
     {
-      private: {
-        GuildRegistry: {
-          address: '0xf0EE9557982b4300a69f355a896Fd3af69e46D32',
-          startBlock: 1,
-        },
-        PermissionRegistry: {
-          address: '0x0E59f2bAe31C0AAeb1cd0a16E883b00A9CD115eE',
-          startBlock: 1,
-        },
-        Create2Deployer: {
-          address: '0xEa8FFf1398336867D0b259E319b49E0D6622311F',
-          startBlock: 1,
-        },
-      },
       gnosis: {
         GuildRegistry: {
           address: '0x7E6EB84621c3bb8455046f380c3934fAf8076158',

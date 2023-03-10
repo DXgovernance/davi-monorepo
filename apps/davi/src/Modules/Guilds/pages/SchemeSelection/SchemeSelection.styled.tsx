3b1ad909c2035c7996ee1812872a972c6348839b
dcrescimbeni <crescimbenidino@gmail.com>
2023-03-10 17:30:41 -0300

    fix: fixed style of "next" button and moved it to sidebar

--- apps/davi/src/Modules/Guilds/pages/SchemeSelection/SchemeSelection.styled.tsx
+++ apps/davi/src/Modules/Guilds/pages/SchemeSelection/SchemeSelection.styled.tsx
@@ -1,7 +1,8 @@
+import styled from 'styled-components';
 import { Divider } from 'components/Divider';
+import { Button } from 'components/primitives/Button';
 import { Box } from 'components/primitives/Layout';
 import { Heading } from 'components/primitives/Typography';
-import styled from 'styled-components';
 
 export const CardContainer = styled(Box)`
   border-radius: ${({ theme }) => theme.radii.curved};
@@ -28,4 +29,20 @@ export const SchemePropertiesGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   column-gap: 67px;
+`;
+
+export const NextButton = styled(Button)`
+  height: 2.5rem;
+  width: 100%;
+  font-weight: 600;
+  font-size: 16px;
+  background-color: ${({ theme }) => theme.colors.yellow};
+  color: ${({ theme }) => theme.colors.bg4};
+
+  :hover {
+    background-color: ${({ theme }) => theme.colors.darkGreen1};
+    border: 1px solid ${({ theme }) => theme.colors.yellow};
+    color: ${({ theme }) => theme.colors.yellow};
+    opacity: 1;
+  }
 `;
--- apps/davi/src/Modules/Guilds/pages/SchemeSelection/SchemeSelection.tsx
+++ apps/davi/src/Modules/Guilds/pages/SchemeSelection/SchemeSelection.tsx
@@ -17,7 +17,7 @@ import {
 } from 'components/SidebarCard';
 import { Divider } from 'components/Divider';
 import { Result, ResultState } from 'components/Result';
-import { Button, IconButton } from 'components/primitives/Button';
+import { IconButton } from 'components/primitives/Button';
 import { RadioInput } from 'components/primitives/Forms/RadioInput';
 import { Box, Flex } from 'components/primitives/Layout';
 import { StyledLink } from 'components/primitives/Links';
@@ -29,6 +29,7 @@ import { PageContainer, SidebarContent } from 'Modules/Guilds/styles';
 import {
   CardContainer,
   CardTitle,
+  NextButton,
   StyledDivider,
 } from './SchemeSelection.styled';
 import { SchemeInfo } from './SchemeInfo';
@@ -132,21 +133,6 @@ const SchemeSelection = () => {
               }
             />
           </Box>
-          <Flex margin="20px 0px" alignItems="flex-end">
-            <StyledLink
-              to={`/${chainName}/${daoId}/create-proposal?subdao=${
-                selectedScheme.id
-              }${discussionId ? '&ref=' + discussionId : ''}`}
-              variant="outline"
-            >
-              <Button
-                variant="primaryWithBorder"
-                data-testid="create-proposal-btn"
-              >
-                {t('createProposal.createProposal')}
-              </Button>
-            </StyledLink>
-          </Flex>
         </div>
         <SidebarContent>
           <SidebarCard
@@ -180,6 +166,19 @@ const SchemeSelection = () => {
                 );
               })}
             </SidebarCardContent>
+            <Divider />
+
+            <Box margin={'24px'}>
+              <StyledLink
+                to={`/${chainName}/${daoId}/create-proposal?subdao=${
+                  selectedScheme.id
+                }${discussionId ? '&ref=' + discussionId : ''}`}
+              >
+                <NextButton data-testid="create-proposal-btn">
+                  {t('schemes.next')}
+                </NextButton>
+              </StyledLink>
+            </Box>
           </SidebarCard>
         </SidebarContent>
       </PageContainer>
@@ -189,8 +188,8 @@ const SchemeSelection = () => {
 
 export default SchemeSelection;
 
-// TODO: separating lines between options
+// TODO: fix radio button on vote showing white...
+// TODO: have a "go back to scheme selection" button in proposal
 // ? since we aren't enforcing scheme unique names, how should we display data to differentiate them?
-// ? have a "go back to scheme selecction" button in proposal?
 // ? in "create proposal" page, show which scheme was selected
-// ? what goes in the text?
+// ? what goes in the scheme selection text?

8eb7fce2b7aaca881519e0f2223928075c681385
dcrescimbeni <crescimbenidino@gmail.com>
2023-03-27 14:17:47 -0300

    ci: moved DXD token transfer to development.js script on dev-scripts

--- packages/dev-scripts/src/development.js
+++ packages/dev-scripts/src/development.js
@@ -1,4 +1,4 @@
-const hre = require("hardhat");
+const hre = require('hardhat');
 
 /**
  * This script is intended to add custom actions to deployed contracts in
@@ -6,17 +6,18 @@ const hre = require("hardhat");
  */
 
 async function main() {
-  console.log("Executing develoment scripts");
+  console.log('Executing develoment scripts');
   // Build develop-related actions here using hre.deployments
   await addDAOAvatarPermissions();
+  await transferTokens();
 }
 
 async function addDAOAvatarPermissions() {
-  const DAOAvatar = await hre.deployments.get("DAOAvatar");
+  const DAOAvatar = await hre.deployments.get('DAOAvatar');
 
-  const PermissionRegistry = await hre.artifacts.require("PermissionRegistry");
+  const PermissionRegistry = await hre.artifacts.require('PermissionRegistry');
   const permissionRegistryDeployed = await hre.deployments.get(
-    "PermissionRegistry"
+    'PermissionRegistry'
   );
   const permissionRegistry = await PermissionRegistry.at(
     permissionRegistryDeployed.address
@@ -24,11 +25,28 @@ async function addDAOAvatarPermissions() {
 
   await permissionRegistry.setETHPermission(
     DAOAvatar.address,
-    "0x0000000000000000000000000000000000000000",
-    "0x00000000",
+    '0x0000000000000000000000000000000000000000',
+    '0x00000000',
     0,
     true
-    );
+  );
+}
+
+async function transferTokens() {
+  const { getNamedAccounts, deployments } = hre;
+  const { tokenHolder, tokenHolder2, tokenHolder3 } = await getNamedAccounts();
+
+  // DXDToken contract
+  const dxdTokenDeployed = await deployments.get('ERC20Mock');
+  const DXDToken = await hre.artifacts.require('ERC20Mock');
+  const dxdToken = await DXDToken.at(dxdTokenDeployed.address);
+
+  await dxdToken.transfer(tokenHolder2, hre.web3.utils.toWei('100'), {
+    from: tokenHolder,
+  });
+  await dxdToken.transfer(tokenHolder3, hre.web3.utils.toWei('100'), {
+    from: tokenHolder,
+  });
 }
 
 main().catch((error) => {
